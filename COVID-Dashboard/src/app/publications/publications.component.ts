3fcb7efce66d2258b3eee7ea8dd81b6701df523d
Leo Velosa <64762864+LeoVelosa@users.noreply.github.com>
2021-04-17 19:21:38 -0400

    Made literature page
    For some reason when I try to deploy this it doesn't update the hosted site

--- COVID-Dashboard/src/app/publications/publications.component.ts
+++ COVID-Dashboard/src/app/publications/publications.component.ts
@@ -9,23 +9,20 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 })
 export class PublicationsComponent {
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+        return {
+          columns: 2,
+          Abstracts: {cols: 2, rows: 2},
+          Visualization: {cols: 2, rows: 2}
+        };
       }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
+      return {
+        columns: 2,
+        Abstracts: {cols: 1, rows: 2},
+        Visualization: {cols: 1, rows: 2}
+      };
     })
   );
 
