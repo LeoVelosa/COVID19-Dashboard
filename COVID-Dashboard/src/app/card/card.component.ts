42966304db2c908e371d998539ebb42d18306397
blaze500 <57372238+blaze500@users.noreply.github.com>
2021-04-20 13:10:14 -0400

    Newsest version, missing database uploader

--- COVID-Dashboard/src/app/card/card.component.ts
+++ COVID-Dashboard/src/app/card/card.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { async } from 'rxjs';
+/*FOR FRONT END YOU NEED THE FOLLOWING CODE FOR FIREBASE AND MAKE SURE FIREBASE GOES INTO THE GET TWEETS COMMAND*/
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/functions';
+/* Gets the function from js.js */
 declare function getTweets(firebase: any): any;
 
 @Component({
@@ -13,8 +15,9 @@ declare function getTweets(firebase: any): any;
 export class CardComponent implements OnInit {
 
   @Input() title!: string;
-
+/*Gets the function from js.js  MUST BE LIKE THIS*/
   ngOnInit(): void {
+    /*puts the firebase api into the getTweets function and it returns html which will go to ___ ID*/
     getTweets(firebase);
   }
 }
--- COVID-Dashboard/src/javaStuff/js.js
+++ COVID-Dashboard/src/javaStuff/js.js
@@ -1,4 +1,6 @@
+//This is the way to get the JSON files and make them into html and sends them to card-component.ts
 async function getTweets(firebase) {
+  //Contains our firebase keys and such to access firebase
   var firebaseConfig = {
     apiKey: "AIzaSyDulDJhTOELGvn4zlBB5PFtf05y19T3yjY",
     authDomain: "fir-test-ebd4c.firebaseapp.com",
@@ -8,20 +10,27 @@ async function getTweets(firebase) {
     appId: "1:498339362637:web:7d99f2f22284507a956904",
     measurementId: "G-1F87Y1K1GB"
   };
-
+  //Initalizes app
   firebase.initializeApp(firebaseConfig);
+  //Firebase acesses firestore
   const db = firebase.firestore();
-
+  //Basically creates a storage for all the html. IT NEEDS TO BE THIS WAY OTHERWISE PAGE WILL REFRESH AND THINGS WILL MESS UP
   const tweets=document.getElementById("test")
+  // Draws data from database and gets a collection of JSON files from it
   var stuff = await db.collection('Twitter_Data').get().then((twitterData) => {
+    // Styles the html, cannot use card-component.css to do this, so it must be done here
     tweets.innerHTML += '<style> ' +
       'div.tweet{ width: 300px; border: 3px solid grey; text-align: left;  padding-left: 15px; padding-top: 10px; border-radius: 5px; height: 100%;}' +
       'img.profile {border: 1px solid black; border-radius: 50%;}' +
       'p.text, div.text {color:black;}' +
       '</style>'
+    //For every Json file in the collection
     twitterData.docs.forEach(doc => {
-      tweets.innerHTML += '<a href="https://www.w3schools.com/html/html_css.asp"><div class="tweet">' +
+      //Gets data from the JSON to make a hyperlink for the box (a) and makes a box (div)
+      tweets.innerHTML += '<a href="https://twitter.com/home"><div class="tweet">' +
+        //Adds profile pic, twitter name and handle
         '<p class="text">' + '<img src='+doc.data().AlachuaCounty.user.profile_image_url_https+' class="profile">  ' +doc.data().AlachuaCounty.user.name+'  @'+doc.data().AlachuaCounty.user.screen_name+' </p>' +
+        //Adds text from tweet
         '<div class="text">'+doc.data().AlachuaCounty.text +'</div>' +
         '</div></a>';
     })}) ;
