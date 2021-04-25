19b192c2eb09fcf7b3b042564e10209611000295
Melanie McCord <melanie.mccord19@ncf.edu>
2021-04-25 15:53:23 -0400

    got author's name and search results

--- COVID-Dashboard/src/pubmed_pulling/pubmed_tester.js
+++ COVID-Dashboard/src/pubmed_pulling/pubmed_tester.js
@@ -17,16 +17,29 @@ firebaseConfig = {
     var searchRef = db.collection(collection_name).doc(doc_id);
     // [START get_document]
     // [START firestore_data_get_as_map]
+    
     var doc = await searchRef.get();
     if (!doc.exists) {
       console.log('No such document');
       return null;
     } else {
-      console.log("Document data", doc.data());
-      console.log(doc.data().Id)
+      // This is the array of search results
+      // console.log("Document data", doc.data().eSummaryResult.DocSum);
+      var search_results = doc.data().eSummaryResult.DocSum;
+      for (var i = 0; i < 25; i++) {
+        console.log(search_results[0]);
+      }
+      // This is the title of the document
+      // console.log(doc.data().eSummaryResult.DocSum[0].Item[5]._);
+      /*
+      for (var i = 0; i < 5; i++)
+        console.log("Entry at", i, " is", doc.data().eSummaryResult.DocSum[i]);
+      */
+      // This is the author's name
       return doc.data();
     }
   }
-  var data = getDocument(db, "covid_pubmed_search", "KrFZZdQN6mmYI3u9cexd");
-  console.log(data);
+  data = getDocument(db, "covid_pubmed_search", "KrFZZdQN6mmYI3u9cexd");
+  console.log("Printing data");
+  console.log("Data", data);
   
