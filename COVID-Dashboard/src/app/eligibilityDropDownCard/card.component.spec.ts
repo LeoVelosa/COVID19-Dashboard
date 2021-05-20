0abaa71667a6422e678a531f00f421f4c487af4a
blaze500 <57372238+blaze500@users.noreply.github.com>
2021-05-20 18:08:30 -0400

    Update card.component.spec.ts

--- COVID-Dashboard/src/app/eligibilityDropDownCard/card.component.spec.ts
+++ COVID-Dashboard/src/app/eligibilityDropDownCard/card.component.spec.ts
@@ -1,4 +1,6 @@
 /* @JM_OneLess And @LV_OneLess (I used his card file and reformatted it to make a specific card so I want to give him credit)  */
+/* This file interacts with every other file in eligibilityDropDownCard. Aside from that, COVID-Dashboard/package.json  */
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EligibilityDropDownCardComponent } from './card.component';
